687a8b682a2c08acfee05bc1b6e9b3d60365e6ab
mindfulmj <58653981+mindfulmj@users.noreply.github.com>
2019-12-08 18:59:42 +0900

    Delete naver_movie_mongo_prac.py

--- naver_movie_mongo_prac.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.dbsparta
-
-target_movie = db.movies.find_one({'title':'사운드 오브 뮤직'})
-target_star = target_movie['star']
-
-db.movies.update_many({'star':target_star},{'$set':{'star':0}})
